5614ba79ed7090f29a6c0103a120b38522d159d3
rohnen <rohnen.raju@edu.ece.fr>
2019-11-28 21:24:17 +0100

    dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,120 @@
-import React from 'react';
+//import React from 'react';
+import React, {Component} from 'react';
+import { Container, Nav } from "./style";
 import logo from './logo.svg';
+import img from './img.png';
 import './App.css';
+import './accueil.css';
+import Widget from './Widget';
+import Widget1 from './Widget1';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+    <Container>
+
+      {/*Menu horizontal tout en haut */}
+      <Nav className="navbar navbar-expand-md is-purple">
+        <Container className="navbar-brand">
+            <a>Online Dashboard</a>
+        </Container>
+        <Container className="navbar-nav ml-auto">
+          <Container className="user-detail-section">
+              <span className="pr-2">
+                <a> Bonjour Etudiant! </a>
+              </span>
+              <span className="logo-search">
+                <img src={img} className="rounded-circle" alt="user" />
+              </span>
+          </Container>
+        </Container>
+      </Nav>
+
+      
+      <Nav className="navbar fixed-top nav-secondary is-dark is-light-text">
+          <Container className="text-medium">Widgets</Container>
+          <Container className="text-medium">Incrementer</Container>
+      </Nav>
+
+      <Container className="container-fluid pr-5 pl-5 pt-5 pb-5">
+          {/*1ère rangée de 4 widgets*/}
+          <Container className="row">
+
+            <Container className="col-lg-3 col-sm-6 is-light-text mb-4">
+              <Container className="card grid-card is-card-dark">
+                <Container className="card-heading">
+                  <Container className="is-dark-text-light letter-spacing text-small">
+                    Nombre d'absences :
+                  </Container>
+                </Container>
+                <p clasName="nbre">20</p>
+              </Container> 
+            </Container> 
+
+            <Container className="col-lg-3 col-sm-6 is-light-text mb-4">
+              <Container className="card grid-card is-card-dark">
+                <Container className="card-heading">
+                  <Container className="is-dark-text-light letter-spacing text-small">
+                    Cours suivant : 
+                  </Container>
+                </Container>  
+                <p clasName="nbre">Cours de design thinking</p>
+              </Container> 
+            </Container>   
+
+            <Container className="col-lg-3 col-sm-6 is-light-text mb-4">
+              <Container className="card grid-card is-card-dark">
+                <Container className="card-heading">
+                  <Container className="is-dark-text-light letter-spacing text-small">
+                    Nombre de retard :
+                  </Container>
+                </Container> 
+                <p clasName="nbre">10</p>
+              </Container> 
+            </Container>   
+
+            <Container className="col-lg-3 col-sm-6 is-light-text mb-4">
+              <Container className="card grid-card is-card-dark">
+                <Container className="card-heading">
+                  <Container className="is-dark-text-light letter-spacing text-small">
+                    Nouvelle Note reçue :
+                  </Container>
+                </Container> 
+                <p clasName="nbre">20</p>
+              </Container> 
+            </Container>
+
+          </Container>  
+          
+          {/*2ème rangée de 2 widgets avec recharts */}
+          <Container className="row" style={{ minHeight: "100%" }}>
+
+            <Container className="col-md-6 mb-4">
+              <Container className="card is-card-dark chart-card">
+                <p className="Widget">Camemebert:</p>
+                <Container className="chart-container large full-height">
+                  <Widget/>
+                </Container>
+              </Container>
+            </Container>  
+
+            <Container className="col-md-6 mb-4">
+              <Container className="card is-card-dark chart-card">
+                <p className="Widget">Camembert:</p>
+                <Container className="chart-container large full-height">
+                  <Widget1/>
+                </Container>
+              </Container>
+            </Container>
+
+          </Container> 
+
+        </Container> 
+
+  </Container>
+
+    );
+  }
 }
 
 export default App;
